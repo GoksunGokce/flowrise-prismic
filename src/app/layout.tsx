2a228485ef64d20840a5fb07a3b8b7114d664b61
goksungokce <goksunb.gokce@gmail.com>
2024-05-14 12:18:41 +0300

    feat:prismic Slice Machine init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,21 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Nunito, Nunito_Sans} from "next/font/google";
 import "./globals.css";
+import clsx from "clsx";
 
-const inter = Inter({ subsets: ["latin"] });
+
+const nunito = Nunito({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+  variable:'--font-nunito'
+});
+const nunito_Sans = Nunito_Sans({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+  variable:'--font-nunito-sans'
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={clsx(nunito.variable, nunito_Sans.variable)}>{children}</body>
     </html>
   );
 }
