830a9ba558870427b77f5ca21bef951afcba7a23
goksungokce <goksunb.gokce@gmail.com>
2024-05-15 11:38:07 +0300

    feat: Add Homepage and pagetype

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,34 @@
+import { Metadata } from "next";
+import { SliceZone } from "@prismicio/react";
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <main className="">
-      <h1 className=" text-5xl font-body font-bold"> Hello world!</h1>
-    </main>
-  );
+import { createClient } from "@/prismicio";
+////import {components} from "@/slices";
+
+export default async function  Page() {
+  const client =createClient();
+  const page=await client.getSingle("homepage");
+  return <div className="text-red-500"> Homepage!</div>
+
+  //return <SliceZone slices={page.data.slices} components={components} />;
+
 }
+
+export async function generateMetadata() : Promise<Metadata> {
+  const client =createClient();
+  const page=await client.getSingle("homepage");
+
+  return {
+    title:page.data.meta_title,
+    description:page.data.meta_description,
+
+  };
+  
+}
+
+
+
+
+
+
+
