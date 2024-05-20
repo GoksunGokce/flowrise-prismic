5ed70801b8621ddf5b6c484337d2631b92ffd7a7
goksungokce <goksunb.gokce@gmail.com>
2024-05-20 10:59:38 +0300

    feat:Create Herp slice and helper components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,13 @@ import { SliceZone } from "@prismicio/react";
 import Image from "next/image";
 
 import { createClient } from "@/prismicio";
-////import {components} from "@/slices";
+import {components} from "@/slices";
 
 export default async function  Page() {
   const client =createClient();
   const page=await client.getSingle("homepage");
-  return <div className="text-red-500"> Homepage!</div>
 
-  //return <SliceZone slices={page.data.slices} components={components} />;
+  return <SliceZone slices={page.data.slices} components={components} />;
 
 }
 
